75d04b324ca4b9a3aba1b5600261c00f2afeb982
Tracey <radcliffe.tracey@gmail.com>
2016-07-24 14:13:59 -0700

    finished test

--- lab-traceyr/test/test_server.js
+++ lab-traceyr/test/test_server.js
@@ -1,1 +1,37 @@
-//need to do this
+'use strict';
+const net = require('net');
+const expect = require('chai').expect;
+
+const server = require('../server');
+const port = 5000;
+
+describe('chat server testing', function(){
+  before(function(done){
+    server.listen(port, done);
+  });
+
+  after(function(done){
+    server.close(done);
+  });
+
+  it('should send messages between clients', function(done){
+    let client1 = net.connect({port});
+    let client2 = net.connect({port});
+    var message = ['test messages', 'Welcome to the CHAT\n'];
+    var send = ['test messages'];
+
+    client2.on('data', function(data){
+      expect(data.toString().replace(/guest-\d+> /, '')).to.eql(message.pop());
+      if(send.length)
+        client1.write(send.pop());
+      else
+        client1.end();
+    });
+
+    client1.on('close', function(){
+      client2.end();
+      expect(message.length).to.eql(0);
+      done();
+    });
+  });
+});
